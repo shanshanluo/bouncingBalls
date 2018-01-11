2b24a1b7930bf50017ceff0d298e767a02d6973a
shanshanluo <shanshanluo295@gmail.com>
2018-01-11 11:44:05 -0600

    add code to check collision and change moving direction after collision

--- bouncingBalls/main.js
+++ bouncingBalls/main.js
@@ -22,6 +22,7 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
+// draw ball
 Ball.prototype.draw = function(){
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -29,6 +30,7 @@ Ball.prototype.draw = function(){
   ctx.fill();
 }
 
+// update location of each ball
 Ball.prototype.update = function() {
   if((this.x + this.size) >= width) {
     this.velX = -(this.velX);
@@ -49,6 +51,30 @@ Ball.prototype.update = function() {
   this.y += this.velY;
 }
 
+Ball.prototype.checkCollision = function() {
+  var distance;
+  for(var i=0; i<balls.length; i++) {
+    if(balls[i] === this) {
+      continue;
+    } else {
+      var dx = this.x - balls[i].x;
+      var dy = this.y - balls[i].y;
+      distance = Math.sqrt(dx*dx + dy*dy);
+      if(distance <= (this.size + balls[i].size)) {
+			  this.velX = -(this.velX);
+				this.velY = -(this.velY);
+				balls[i].velX = -(balls[i].velX);
+				balls[i].velY = -(balls[i].velY);
+				this.x += this.velX;
+				this.y += this.velY;
+				balls[i].x += balls[i].velX;
+				balls[i].y += balls[i].velY;
+        this.color = balls[i].color = 'rgb(' + random(0,255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+      }
+    }
+  }
+}
+
 var balls = [];
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
@@ -57,15 +83,17 @@ function loop() {
   while(balls.length < 25) {
     var ball = new Ball(random(0, width),
                 random(0, height),
-                random(-7, 7), random(-7, 7),
+                random(1, 7), random(1, 7),
                 'rgb(' + random(0,255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
                 random(10, 20));
     balls.push(ball);
   }
 
   for(var i = 0; i < balls.length; i++) {
+	balls[i].checkCollision();
     balls[i].draw();
     balls[i].update();
+    
   }
 
   requestAnimationFrame(loop);
